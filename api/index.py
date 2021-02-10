b2b4309eb0946a9f4b266ee56cbdf60f38622ef0
Kev <kevin.mach88@gmail.com>
2021-02-09 22:40:10 -0800

    add requets

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 # from cowpy import cow
+from datetime import datetime
 from yahoo_fin import stock_info as si
 
 class handler(BaseHTTPRequestHandler):
@@ -8,6 +9,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
+        self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
         # message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
         # self.wfile.write(message.encode())
         message = 'GME ' + str(si.get_live_price("GME")) + ' 🚀🚀🚀🚀🚀🚀🚀🚀🚀'
